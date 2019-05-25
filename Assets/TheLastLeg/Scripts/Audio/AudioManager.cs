640de069e1fbeb48c6ba4cbb9ce48a27a324b33f
Jared MacDonald <jared.macdonald@snowedin.ca>
2019-05-25 19:01:55 -0400

    Added the beginnings of a Main menu. Added a simple MenuManager to be able to play game and quit. Added a AudioManager to manager playing all sounds. Added some UI click and hover sounds. Moved HealthUI.cs to proper scripts folder.

---
+++ Assets/TheLastLeg/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+        static AudioManager _instance;
+    public static AudioManager Instance
+    {
+        get { return _instance; }
+    }
+
+    public Sound[] sounds;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        if (_instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+
+        foreach (Sound sound in sounds)
+        {
+            sound.audioSource = gameObject.AddComponent<AudioSource>();
+            sound.audioSource.clip = sound.audioClip;
+            sound.audioSource.volume = sound.volume;
+            sound.audioSource.pitch = sound.pitch;
+            sound.audioSource.loop = sound.looping;
+        }
+	}
+	
+	public void PlaySound(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if(s != null)
+        {
+            s.audioSource.Play();
+        }
+    }
+}
---
+++ Assets/TheLastLeg/Scripts/MenuManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioManager audioManager;
+    
+    public void PlayGame()
+    {
+        audioManager.PlaySound("Click");
+
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+    }
+
+    public void OnButtonHover()
+    {
+        audioManager.PlaySound("Hover");
+    }
+
+    public void QuitGame()
+    {
+        audioManager.PlaySound("Click");
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
