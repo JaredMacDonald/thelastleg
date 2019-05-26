a6a6962088fd73dac1a23d53fc293986dfe7ecf2
Jared MacDonald <jared.macdonald@snowedin.ca>
2019-05-26 18:41:43 -0400

    Added Fire High images. Updated the End Flag to use the new flag. Added a simple SpriteAnimation script to animate non-player sprites.

---
+++ Assets/TheLastLeg/Scripts/SpriteAnimation.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteAnimation : MonoBehaviour
+{
+    private int animationFrameIndex;
+    private float animationFrameTime;
+
+    private float animationFPS = 10f;
+
+    public Sprite[] Frames;
+
+    private SpriteRenderer _Renderer;
+    private SpriteRenderer Renderer
+    {
+        get
+        {
+            if (_Renderer == null)
+            {
+                _Renderer = GetComponent<SpriteRenderer>();
+            }
+            return _Renderer;
+        }
+    }
+
+    void Update()
+    {
+        if (Frames.Length == 0)
+        {
+            return;
+        }
+
+        float frameTimeMax = 1f / animationFPS;
+        animationFrameTime += Time.deltaTime;
+
+        if (animationFrameTime > frameTimeMax || animationFrameIndex == -1)
+        {
+            animationFrameIndex = (animationFrameIndex + 1) % Frames.Length;
+            animationFrameTime = 0f;
+
+            Renderer.sprite = Frames[animationFrameIndex];
+        }
+    }
+
+    public void SetAnimationFPS(float fps)
+    {
+        animationFPS = fps;
+    }
+}
